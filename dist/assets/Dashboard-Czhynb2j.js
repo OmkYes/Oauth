515f9ebdbebfef1fc2a48ad646b1c3b79f2d2f55
omkar <omkaryeske1@gmail.com>
2025-03-20 16:32:02 +0530

    frontend added

---
+++ dist/assets/Dashboard-Czhynb2j.js
@@ -0,0 +1,1 @@
+import{g as n,a as r,j as t}from"./index-B7HqHYw3.js";const u=()=>{const[s]=n(),{user:e}=r(o=>o.auth);return t.jsx(t.Fragment,{children:t.jsxs("div",{style:{padding:10,display:"flex",justifyContent:"space-between"},children:[t.jsx("strong",{children:e.name}),t.jsx("button",{onClick:s,children:"logout"})]})})};export{u as default};
---
+++ dist/assets/Protected-BvsrIyjx.js
@@ -0,0 +1,1 @@
+import{a as o,j as a,N as r}from"./index-B7HqHYw3.js";const n=({children:t})=>{const{user:e}=o(s=>s.auth);return e?t:a.jsx(r,{to:"/login"})};export{n as default};
